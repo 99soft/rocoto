d66b694c57d39325bee4d6abceced9b2d32dcb10
Simone Tripodi <simone.tripodi@gmail.com>
2010-08-20 09:19:40 +0000

    removed the useless rocoto-core dependency

--- simple-configuration/src/main/java/com/googlecode/rocoto/simpleconfig/PropertiesReader.java
+++ simple-configuration/src/main/java/com/googlecode/rocoto/simpleconfig/PropertiesReader.java
@@ -16,45 +16,118 @@
 package com.googlecode.rocoto.simpleconfig;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Properties;
-
-import com.googlecode.rocoto.core.AbstractURLReader;
 
 /**
  * 
  * @author Simone Tripodi
  * @version $Id$
  */
-final class PropertiesReader extends AbstractURLReader<Properties> {
+final class PropertiesReader {
+
+    private final URL url;
 
     private final boolean isXML;
 
-    public PropertiesReader(File file, boolean isXML) {
-        super(file);
-        this.isXML = isXML;
-    }
-
+    /**
+     * 
+     * @param classpathResource
+     * @param classLoader
+     */
     public PropertiesReader(String classpathResource, ClassLoader classLoader, boolean isXML) {
-        super(classpathResource, classLoader);
-        this.isXML = isXML;
-    }
-
-    public PropertiesReader(URL url, boolean isXML) {
-        super(url);
-        this.isXML = isXML;
-    }
-
-    @Override
-    protected Properties process(InputStream input) throws Exception {
-        Properties properties = new Properties();
-        if (this.isXML) {
-            properties.loadFromXML(input);
-        } else {
-            properties.load(input);
+        if (classpathResource == null) {
+            throw new IllegalArgumentException("'classpathResource' argument can't be null");
         }
-        return properties;
+        if (classLoader == null) {
+            throw new IllegalArgumentException("'classLoader' argument can't be null");
+        }
+
+        if ('/' == classpathResource.charAt(0)) {
+            classpathResource = classpathResource.substring(1);
+        }
+
+        URL url = classLoader.getResource(classpathResource);
+        if (url == null) {
+            throw new IllegalArgumentException("classpath resource '"
+                    + classpathResource
+                    + "' doesn't exist");
+        }
+        this.url = url;
+        this.isXML = isXML;
+    }
+
+    /**
+     * 
+     * @param file
+     */
+    public PropertiesReader(File file, boolean isXML) {
+        if (file == null) {
+            throw new IllegalArgumentException("'configurationFile' argument can't be null");
+        }
+        if (!file.exists()) {
+            throw new RuntimeException("Impossible to read file '"
+                    + file.getAbsolutePath()
+                    + " because it doesn't exist");
+        }
+
+        try {
+            this.url = file.toURI().toURL();
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Impossible to load properties file '"
+                    + file.getAbsolutePath()
+                    + ", see nested exceptions", e);
+        }
+        this.isXML = isXML;
+    }
+
+    /**
+     * 
+     * @param url
+     */
+    public PropertiesReader(URL url, boolean isXML) {
+        if (url == null) {
+            throw new IllegalArgumentException("'url' argument can't be null");
+        }
+        this.url = url;
+        this.isXML = isXML;
+    }
+
+    /**
+     * 
+     * @return
+     */
+    public Properties read() throws Exception {
+        URLConnection connection = null;
+        InputStream input = null;
+        try {
+            connection = url.openConnection();
+            input = connection.getInputStream();
+
+            Properties properties = new Properties();
+            if (this.isXML) {
+                properties.loadFromXML(input);
+            } else {
+                properties.load(input);
+            }
+            return properties;
+        } finally {
+            if (connection != null && (connection instanceof HttpURLConnection)) {
+                ((HttpURLConnection) connection).disconnect();
+            }
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    // close quietly
+                }
+            }
+        }
     }
 
 }
