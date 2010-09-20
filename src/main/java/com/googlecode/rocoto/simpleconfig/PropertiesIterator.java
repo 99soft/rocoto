042a0f9122a75daeea0361321899432bc19aa83a
Simone Tripodi <simone.tripodi@gmail.com>
2010-09-20 18:43:40 +0000

    first checkin of PropertiesIterator class

---
+++ src/main/java/com/googlecode/rocoto/simpleconfig/PropertiesIterator.java
@@ -0,0 +1,69 @@
+/*
+ *    Copyright 2009-2010 The Rocoto Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package com.googlecode.rocoto.simpleconfig;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ * 
+ * @author Simone Tripodi
+ * @since 3.2
+ * @version $Id$
+ */
+final class PropertiesIterator implements Iterator<Entry<String, String>> {
+
+    private final String keyPrefix;
+
+    private final Iterator<Entry<Object, Object>> properties;
+
+    public PropertiesIterator(Map<Object, Object> properties) {
+        this(null, properties);
+    }
+
+    public PropertiesIterator(String keyPrefix, Map<Object, Object> properties) {
+        this.keyPrefix = keyPrefix;
+        this.properties = properties.entrySet().iterator();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public boolean hasNext() {
+        return this.properties.hasNext();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Entry<String, String> next() {
+        Entry<Object, Object> next = this.properties.next();
+        String key = String.valueOf(next.getKey());
+        if (this.keyPrefix != null && this.keyPrefix.length() > 0) {
+            key = this.keyPrefix + key;
+        }
+        return new KeyValue(key, String.valueOf(next.getValue()));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void remove() {
+        // not needed in this version
+    }
+
+}
