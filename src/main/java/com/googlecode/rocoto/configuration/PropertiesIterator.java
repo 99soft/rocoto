f5983215622ff06fed351216b797a5c089000f7c
Simone Tripodi <simone.tripodi@gmail.com>
2010-10-04 16:02:41 +0000

    no needs to throw exceptions

--- src/main/java/com/googlecode/rocoto/configuration/PropertiesIterator.java
+++ src/main/java/com/googlecode/rocoto/configuration/PropertiesIterator.java
@@ -30,6 +30,6 @@ public interface PropertiesIterator {
      * 
      * @return
      */
-    Iterator<Entry<String, String>> read() throws Exception;
+    Iterator<Entry<String, String>> iterator();
 
 }
