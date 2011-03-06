654fb5a30e6adaaf89e1493660466f46d979d5c3
simonetripodi <simone.tripodi@gmail.com>
2011-03-06 18:02:49 +0100

    first checkin of PropertyValueBindingBuilder interface

---
+++ src/main/java/org/nnsoft/guice/rocoto/configuration/PropertyValueBindingBuilder.java
@@ -0,0 +1,30 @@
+/*
+ *    Copyright 2009-2011 The Rocoto Team
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.nnsoft.guice.rocoto.configuration;
+
+/**
+ * Binds to a property value.
+ */
+public interface PropertyValueBindingBuilder {
+
+    /**
+     * Binds property to the given value.
+     *
+     * @param value The property value
+     */
+    void toValue(String value);
+
+}
